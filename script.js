3566e1eb422bb70b1bfb55c29692e1fb9fc212da
kfigaj <kfigaj87@gmail.com>
2023-05-22 21:00:59 +0200

    popIt commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
+const btnHeader = document.querySelector(".btn-header");
+
+btnHeader.addEventListener("click", function () {
+  document.querySelector("header").classList.add("new-header-background");
+});
